65449edc0d6517a9c060f5bc27f69ed5edd72f0d
Shaheer <shaheerahmaddev@gmail.com>
2024-08-02 20:17:18 +0500

    Add port to env

--- main.py
+++ main.py
@@ -136,4 +136,5 @@ def evaluate_quiz(request: EvaluateRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    porttoRun = os.getenv('PORT')
+    uvicorn.run(app, host="0.0.0.0", port=porttoRun)
