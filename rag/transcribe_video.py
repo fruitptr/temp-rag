46bde3896ae0cfbcf281a75d0c2366e6db259c54
Shaheer <shaheerahmaddev@gmail.com>
2024-08-10 20:50:04 +0500

    Use pytube

--- rag/transcribe_video.py
+++ rag/transcribe_video.py
@@ -1,9 +1,9 @@
 import os
 import json
 from dotenv import load_dotenv
-# from pytubefix import YouTube
+from pytubefix import YouTube
 from openai import OpenAI
-import yt_dlp
+# import yt_dlp
 
 load_dotenv()
 
@@ -11,27 +11,27 @@ def transcribe_video(youtubelink):
     print("Video to audio...")
     # Convert Youtube video to audio
     try:
-        # video = YouTube(youtubelink)
-        # print("video object", video)
-        # print("video streams", video.streams)
-        # stream = video.streams.filter(only_audio=True).first()
-        # print("stream object", stream)
-        # stream.download(filename="audio.mp3")
-        # print("Audio File downloaded in MP3")
+        video = YouTube(youtubelink, use_oauth=True, allow_oauth_cache=True)
+        print("video object", video)
+        print("video streams", video.streams)
+        stream = video.streams.filter(only_audio=True).first()
+        print("stream object", stream)
+        stream.download(filename="audio.mp3")
+        print("Audio File downloaded in MP3")
 
-        ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'outtmpl': 'audio',
-        }
+        # ydl_opts = {
+        # 'format': 'bestaudio/best',
+        # 'postprocessors': [{
+        #     'key': 'FFmpegExtractAudio',
+        #     'preferredcodec': 'mp3',
+        #     'preferredquality': '192',
+        # }],
+        # 'outtmpl': 'audio',
+        # }
         
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([youtubelink])
-            print("Audio downloaded successfully as audio.mp3")
+        # with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        #     ydl.download([youtubelink])
+        #     print("Audio downloaded successfully as audio.mp3")
 
         client = OpenAI()
 
