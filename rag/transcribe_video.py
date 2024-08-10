aee91e53cde55aa1d86626931decc88b0bc2bb8a
Shaheer <shaheerahmaddev@gmail.com>
2024-08-10 19:02:08 +0500

    Use yt-dlp instead of pytubefix

--- rag/transcribe_video.py
+++ rag/transcribe_video.py
@@ -1,8 +1,9 @@
 import os
 import json
 from dotenv import load_dotenv
-from pytubefix import YouTube
+# from pytubefix import YouTube
 from openai import OpenAI
+import yt_dlp
 
 load_dotenv()
 
@@ -10,13 +11,27 @@ def transcribe_video(youtubelink):
     print("Video to audio...")
     # Convert Youtube video to audio
     try:
-        video = YouTube(youtubelink)
-        print("video object", video)
-        print("video streams", video.streams)
-        stream = video.streams.filter(only_audio=True).first()
-        print("stream object", stream)
-        stream.download(filename="audio.mp3")
-        print("Audio File downloaded in MP3")
+        # video = YouTube(youtubelink)
+        # print("video object", video)
+        # print("video streams", video.streams)
+        # stream = video.streams.filter(only_audio=True).first()
+        # print("stream object", stream)
+        # stream.download(filename="audio.mp3")
+        # print("Audio File downloaded in MP3")
+
+        ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': 'audio',
+        }
+        
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([youtubelink])
+            print("Audio downloaded successfully as audio.mp3")
 
         client = OpenAI()
 
